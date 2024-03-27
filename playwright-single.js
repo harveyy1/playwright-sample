72126083c15819b3ceb46f952f16d3e5d72848f8
Harvey Prewer <harvey.prewer@focusrite.com>
2024-03-27 17:56:46 +0000

    the search bar selector was wrong

--- playwright-single.js
+++ playwright-single.js
@@ -1,7 +1,5 @@
 const { chromium } = require('playwright')
-const {expect} = require("expect");
-const cp = require('child_process');
-const playwrightClientVersion = cp.execSync('npx playwright --version').toString().trim().split(' ')[1];
+const { expect } = require('@playwright/test');
 
 (async () => {
   const capabilities = {
@@ -9,17 +7,13 @@ const playwrightClientVersion = cp.execSync('npx playwright --version').toString
     'browserVersion': 'latest',
     'LT:Options': {
       'platform': 'Windows 10',
-      'build': 'Playwright Single Build',
+      'build': 'Playwright Sample Build',
       'name': 'Playwright Sample Test',
-      'user': process.env.LT_USERNAME,
-      'accessKey': process.env.LT_ACCESS_KEY,
+      'user': 'harvey.prewer',
+      'accessKey': 'PR5vz7C7ATNow9Sx0vX5ypBtK0sX4MHhp39Xg6qv7Tmk3OZ6kg',
       'network': true,
       'video': true,
-      'console': true,
-      'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
-      'tunnelName': '', // Optional
-      'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-      'playwrightClientVersion': playwrightClientVersion
+      'console': true
     }
   }
 
@@ -29,28 +23,21 @@ const playwrightClientVersion = cp.execSync('npx playwright --version').toString
 
   const page = await browser.newPage()
 
-  await page.goto("https://duckduckgo.com");
+  await page.goto('https://www.bing.com')
 
-  let element = await page.locator("[name=\"q\"]");
-  await element.click();
-  await element.type("LambdaTest");
-  await element.press("Enter");
+  const element = await page.$('[aria-label="Search the web"]')
+  await element.click()
+  await element.type('LambdaTest')
+  await element.press('Enter')
   const title = await page.title()
 
   try {
-    expect(title).toEqual('LambdaTest at DuckDuckGo')
+    expect(title).toEqual('LambdaTest - Search')
     // Mark the test as completed or failed
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
-    await teardown(page, browser)
-  } catch (e) {
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: e.stack } })}`)
-    await teardown(page, browser)
-    throw e
+  } catch {
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Title not matched' } })}`)
   }
 
+  await browser.close()
 })()
-
-async function teardown(page, browser) {
-  await page.close();
-  await browser.close();
-}
