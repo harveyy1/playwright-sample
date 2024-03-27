e6e13a7a9c2dbee853ed51a8fd19357ba5e6b2b2
Harvey Prewer <harvey.prewer@focusrite.com>
2024-03-27 18:06:10 +0000

    got focusrite test working as a single one

--- playwright-single.js
+++ playwright-single.js
@@ -7,7 +7,7 @@ const { expect } = require('@playwright/test');
     'browserVersion': 'latest',
     'LT:Options': {
       'platform': 'Windows 10',
-      'build': 'Playwright Sample Build',
+      'build': 'Focusrite Web',
       'name': 'Playwright Sample Test',
       'user': 'harvey.prewer',
       'accessKey': 'PR5vz7C7ATNow9Sx0vX5ypBtK0sX4MHhp39Xg6qv7Tmk3OZ6kg',
@@ -23,21 +23,16 @@ const { expect } = require('@playwright/test');
 
   const page = await browser.newPage()
 
-  await page.goto('https://www.bing.com')
+  await page.goto('https://downloads.focusrite.com/focusrite');
 
-  const element = await page.$('[aria-label="Search the web"]')
-  await element.click()
-  await element.type('LambdaTest')
-  await element.press('Enter')
-  const title = await page.title()
+  await page.getByRole('button', { name: 'Allow all cookies' }).click();
+  
 
-  try {
-    expect(title).toEqual('LambdaTest - Search')
-    // Mark the test as completed or failed
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
-  } catch {
-    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Title not matched' } })}`)
-  }
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get Novation downloads' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Novation Downloads' })).toBeVisible();
 
   await browser.close()
 })()
